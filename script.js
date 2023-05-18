1bee910c31d9e0a6014acb072a8828bc469db100
Владислав <127604284+DossHAK@users.noreply.github.com>
2023-05-18 20:51:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+﻿
+let myForm = document.forms.myForm;
+
+myForm.addEventListener('submit', function() {
+    event.preventDefault();
+
+    let title = document.querySelector('#name').value;
+
+    document.querySelector('.tasks').innerHTML += `
+    <li class="list-group-item"><span>❌</span> ${title}</li>
+    `;
+    myForm.reset();
+});
+
+document.querySelector('.tasks').addEventListener('click', function() {
+    if(event.target.tagName == 'SPAN') {
+        event.target.parentElement.remove();
+    }
+});
+
+
+document.querySelector('#clear').addEventListener('click', function() {
+    document.querySelector('.tasks').innerHTML = '';
+});
+
+
+document.querySelector('#bg').addEventListener('click', function() {
+    document.querySelector('body').style.backgroundColor = `${event.target.dataset.color}`;
+});
